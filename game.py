8fdf187d040cb1ee50d854846fecc7babb077f7e
morganwashington-pixel <morgan.washington@ascte.org>
2026-09-11 11:26:59 -0500

    Update game.py
    added gobo

--- game.py
+++ game.py
@@ -14,7 +14,8 @@ def main():
     goblinTwo = Goblin("Scribble")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
-    
+    print(f"{goblinTwo.name} enters the arena with {goblinTwo.health} health.")
+        
     print("But no hero has answered the call... yet.")
 
 
