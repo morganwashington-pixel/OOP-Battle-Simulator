975def5445a7fc48d4f87082e9cba079e9e1337c
morganwashington-pixel <morgan.washington@ascte.org>
2026-09-14 21:55:27 -0500

    added hero

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 from goblin import Goblin
-
+from hero import Hero
 
 ARENA_NAME = "The Iron Square"
 
@@ -8,15 +8,23 @@ def main():
     """Open the arena and introduce its first opponent."""
     print(f"Welcome to {ARENA_NAME}!")
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
-    print("The gates are opening... \n Are you ready for the challenge?")
+    print("The gates are opening... \nAs you enter the lights turn on and the crowd is waiting for an epic battle \nAre you ready for the challenge?")
 
     goblin = Goblin("GreenGoblin")
     goblinTwo = Goblin("Scribble")
 
     print(f"{goblin.name} enters the arena with {goblin.health} health.")
     print(f"{goblinTwo.name} enters the arena with {goblinTwo.health} health.")
-        
+   
     print("But no hero has answered the call... yet.")
+
+    hero = Hero("Spooderman")
+
+    print(f"{hero.name} enters the arena with {hero.health} health.")
+
+    hero_giveDamage = hero.attack()
+    goblin.take_damage(hero_giveDamage)
+            
 
 
 if __name__ == "__main__":
--- hero.py
+++ hero.py
@@ -1,4 +1,25 @@
+import random
 class Hero:
     """The hero blueprint will be implemented later in the project."""
 
-    pass
+    def __init__ (self,name):
+        self.name = name 
+        self.health = 110
+        self.attack_power = 20
+        
+
+    def attack(self):
+        return random.randint(1,20)
+
+    def take_damage(self, damage):
+    # Subtract damage, but do not allow health to fall below 0.
+        self.health =  max(0, self.health - damage)
+
+        
+    def is_alive(self):
+    # Return a Boolean based on this Hero's health.
+        if self.health > 0:
+            return True 
+        else: 
+            return False 
+
