4b1f1a87803badd0c7b2552db4226e867ff27a16
morganwashington-pixel <morgan.washington@ascte.org>
2026-09-15 08:55:49 -0500

    Update game.py

--- game.py
+++ game.py
@@ -3,12 +3,25 @@ from hero import Hero
 
 ARENA_NAME = "The Iron Square"
 
+def battle(hero: Hero, enemy: Goblin):
+
+    while hero.is_alive() and enemy.is_alive():
+        hero_damage = hero.attack()
+        enemy.take_damage(hero_damage)
+        if enemy.is_alive():
+            enemy_damage = enemy.attack()
+            hero.take_damage(enemy_damage)
+        if hero.is_alive():
+            print(f"{hero.name} wins!")
+        else:
+            print(f"{enemy.name} wins!")
+
 
 def main():
     """Open the arena and introduce its first opponent."""
     print(f"Welcome to {ARENA_NAME}!")
     print("༼ ᓄºل͟º ༽ᓄ   ᕦ(ò_óˇ)ᕤ")
-    print("The gates are opening... \nAs you enter the lights turn on and the crowd is waiting for an epic battle \nAre you ready for the challenge?")
+    print("The gates are opening... \nAs you enter, the lights turn on \nThe crowd is waiting for an epic battle \nAre you ready for the challenge?")
 
     goblin = Goblin("GreenGoblin")
     goblinTwo = Goblin("Scribble")
@@ -24,7 +37,7 @@ def main():
 
     hero_giveDamage = hero.attack()
     goblin.take_damage(hero_giveDamage)
-            
+    battle(hero, goblin)
 
 
 if __name__ == "__main__":
